a0aecf2f51bf88ceb6b3e185e027551514640b62
Elly <emorri08@seattlecentral.edu>
2018-04-06 19:16:12 -0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!--footer starts here-->
+            </section>
+    <footer>
+        <small>&copy; <?=date('Y')?>, Eleanor Boyd, All Rights Reserved, 
+            <a href="http://validator.w3.org/check?uri=referer" target="_blank">Valid HTML
+            </a> ~ 
+            <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">Valid CSS
+            </a>
+        </small>
+    </footer>   
+</body>    
+</html>
---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Eleanor Boyd's ITC240 Staging Area</title>
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="viewport" content="width=device-width" />
+            <link rel="stylesheet" href="css/main.css" />
+            <link rel="stylesheet" href="css/nav.css" />
+                <script src="js/nav.js"></script>
+                <script src='https://www.google.com/recaptcha/api.js'></script> 
+    </head> 
+<body>
+    <h1>Eleanor Boyd: ITC240</h1>
+<!--header ends here-->
